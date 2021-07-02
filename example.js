486c50be128f053490376984df423fca94d952ee
Natsu <75367930+Natsu062@users.noreply.github.com>
2021-07-02 17:27:00 +0700

    Create example.js

---
+++ example.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+
+(async () => {
+    const endpoint = 'https://natsu.yuu02.repl.co/api/riie/search/'
+    const query = 'naruto'
+    
+    fetch(`${animeSearchAPI}${query}`).then(response.json())
+    .then(res => {
+        console.log(res);
+    })
+})
